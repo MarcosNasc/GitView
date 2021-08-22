fd1b300186c782fc74699765e09ebac55a612d8a
Marcos Nascimento <marcosnascimento.dev@gmail.com>
2021-08-22 17:21:04 -0300

    adciona projeto-base

---
+++ GitView/GitView/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitView.Controllers
+{
+    public class BaseController : Controller
+    {
+        public IConfiguration _configuration { get; set; }
+        public BaseController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public void SetBaseConfigPublic()
+        {
+            ViewData["GitUser"] = _configuration["GitUser"];
+        }
+    }
+}
